6c921068da1ef0dc334002b11f26c486ebc41abb
Kevin Oberlag <koberlag@gmail.com>
2015-09-14 18:14:07 -0500

    Update 2015-09-14T23:14:06.972Z

--- js/components/graphics/pipe.js
+++
@@ -1,9 +0,0 @@
-var PipeGraphicsComponent = function(entity) {
-    this.entity = entity;
-};
-
-PipeGraphicsComponent.prototype.draw = function() {
-    console.log("Drawing a pipe");
-};
-
-exports.PipeGraphicsComponent = PipeGraphicsComponent;

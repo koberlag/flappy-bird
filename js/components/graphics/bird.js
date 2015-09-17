f7cf2d405c4c85f3f8e58a8a914cfe5ecbaf3f19
Kevin Oberlag <koberlag@gmail.com>
2015-09-16 21:07:55 -0500

    added updates for scaling and translating the canvas

--- js/components/graphics/bird.js
+++ js/components/graphics/bird.js
@@ -1,25 +1,16 @@
 var BirdGraphicsComponent = function(entity) {
     this.entity = entity;
-    this.position = 0;
 };
 
-BirdGraphicsComponent.prototype.draw = function(context) {	
-    var canvas = document.getElementById('main-canvas')
-    this.position++;
-    if(this.position > canvas.width)
-    {
-        this.position = 0;
-    }
-
+BirdGraphicsComponent.prototype.draw = function(context,position, size) {	
+    context.save();
+    context.translate(position.x, position.y);
+    context.scale(size, size);
     context.beginPath();
     context.fillStyle = "rgb(200,0,0)";
     context.arc(this.position, 500, 50, 0, 2 * Math.PI);
     context.fill();
-    context.fillStyle = "rgb(200,0,0)";
-    context.arc(500 + this.position, 500, 50, 0, 2 * Math.PI);
-    context.fill();
-    context.fillStyle = "rgb(100,0,0)";
-    context.fillRect(10, 10 + this.position, 55, 50);
+    context.restore();
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;

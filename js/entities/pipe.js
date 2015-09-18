2219d2a1914ee72c4d86a039c9e9f8c3c13f1f18
Kevin Oberlag <koberlag@gmail.com>
2015-09-18 08:21:02 -0500

    created pipes that move from right to left

--- js/entities/pipe.js
+++ js/entities/pipe.js
@@ -1,12 +1,19 @@
 var graphicsComponent = require("../components/graphics/pipe");
+var physicsComponent = require("../components/physics/physics");
 
 var Pipe = function() {
-    console.log("Creating Pipe entity");
+    var physics = new physicsComponent.PhysicsComponent(this);
+    physics.position.y = 0.5;
+    physics.acceleration.y = 0;
+    physics.velocity.x = -0.3;
+
 
     var graphics = new graphicsComponent.PipeGraphicsComponent(this);
     this.components = {
+    	physics: physics,
         graphics: graphics
     };
 };
 
 exports.Pipe = Pipe;
+
--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -6,7 +6,7 @@ var bird = require('./entities/bird');
 var pipe = require('./entities/pipe');
 
 var FlappyBird = function() {
-    this.entities = [new bird.Bird()];
+    this.entities = [new bird.Bird(), new pipe.Pipe()];
     this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
 	this.physics = new physicsSystem.PhysicsSystem(this.entities);
 	this.input = new inputSystem.InputSystem(this.entities);
